b3ab699194b6e36768cfa76eaf80a39339d7bea9
s12553 <s12553@fk1ss.edu.hk>
2025-06-10 15:04:18 +0800

    Update validation.js

--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@ function validateForm(event) {
     event.preventDefault(); // 阻止表单默认提交行为
     const password = document.getElementById("password").value;
 
-    if (password === "123456") { // 验证密码是否正确
+    if (password === "AngerAndHowlingNoOneKnowsForSure") { // 验证密码是否正确
         alert("✅ ");
         document.getElementById("overlay").style.display = "hidden"; // 隐藏覆盖层
     } else {
